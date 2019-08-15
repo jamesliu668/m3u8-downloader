f0c1e2b5720547862956a11bdbdea00be556f0e3
James Liu <james.liu668@aliyun.com>
2019-08-15 15:20:10 +0800

    make it easy to get m3u8 url

--- test/newcoder-downloader-test.py
+++ test/newcoder-downloader-test.py
@@ -56,12 +56,15 @@ if __name__ == '__main__':
         "Origin":"https://www.nowcoder.com",
         "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36",
         "Referer" : "https://www.nowcoder.com/study/vod/1041/1/1",
-        "cookie":"NOWCODERCLINETID=7E576843AAD6A48C69C4FEC9C5D2784F; _ga=GA1.2.271210318.1552288354; NOWCODERUID=817D390DB96D8CAE2C9F7E1E554F6A28; gdxidpyhxdE=uGcyhoRXafAcuWdzdtT8vRBgDHVmvYC5AV82O5MCXuukixnAP%2B6pfRN3kHbWRKlICIEDCst7Za5I6ID%2BiEcuz7b2WEaIvIg1y4y9UpkaAE%2F0MQvys%5CZdf1Y0ovff398cpGkbSwnfLZ9GL1uogPjEkQG0JMkOHCpsr2pq8lTfOw8Jp5dI%3A1563357666843; _9755xjdesxxd_=32; gr_user_id=4505d2e5-9b6b-4064-a202-40e66dd0432b; c196c3667d214851b11233f5c17f99d5_gr_last_sent_cs1=113433790; grwng_uid=6097ead7-9a65-4d18-9b1e-2ad15975cd9b; callBack=%2Fstudy%2Fvod%2F1041%2F10%2F1%3F; Hm_lvt_a808a1326b6c06c437de769d1b85b870=1563152190,1563351717,1563418306,1563756842; t=EEDBC0F292E74BB5A30962D8022311BC; c196c3667d214851b11233f5c17f99d5_gr_session_id=0313e000-0518-4bc8-aed4-3f38a9aea1cc; c196c3667d214851b11233f5c17f99d5_gr_last_sent_sid_with_cs1=0313e000-0518-4bc8-aed4-3f38a9aea1cc; c196c3667d214851b11233f5c17f99d5_gr_cs1=113433790; Hm_lpvt_a808a1326b6c06c437de769d1b85b870=1563756850; c196c3667d214851b11233f5c17f99d5_gr_session_id_0313e000-0518-4bc8-aed4-3f38a9aea1cc=true; SERVERID=aff739a092fc0d444b24c3a30d4864b6|1563756856|1563756841"
+        "cookie":"NOWCODERCLINETID=7E576843AAD6A48C69C4FEC9C5D2784F; _ga=GA1.2.271210318.1552288354; NOWCODERUID=817D390DB96D8CAE2C9F7E1E554F6A28; gdxidpyhxdE=uGcyhoRXafAcuWdzdtT8vRBgDHVmvYC5AV82O5MCXuukixnAP%2B6pfRN3kHbWRKlICIEDCst7Za5I6ID%2BiEcuz7b2WEaIvIg1y4y9UpkaAE%2F0MQvys%5CZdf1Y0ovff398cpGkbSwnfLZ9GL1uogPjEkQG0JMkOHCpsr2pq8lTfOw8Jp5dI%3A1563357666843; _9755xjdesxxd_=32; gr_user_id=4505d2e5-9b6b-4064-a202-40e66dd0432b; c196c3667d214851b11233f5c17f99d5_gr_last_sent_cs1=113433790; grwng_uid=6097ead7-9a65-4d18-9b1e-2ad15975cd9b; c196c3667d214851b11233f5c17f99d5_gr_cs1=113433790; callBack=%2Fstudy%2Fvod%2F1041%2F10%2F1%3F; Hm_lvt_a808a1326b6c06c437de769d1b85b870=1565079570,1565079677,1565079699,1565851742; t=EEB92FD2F4FE354737D792BDAF3B44E7; Hm_lpvt_a808a1326b6c06c437de769d1b85b870=1565851747; SERVERID=aff739a092fc0d444b24c3a30d4864b6|1565851784|1565851739"
     })
 
-    m3u8URL = "https://hls.videocc.net/c7d3982d0d/f/c7d3982d0d4360a289c58754dbdfd80f_2.m3u8?pid=1563756868775X1523162&device=desktop"
+    catcount = 10
+    # m3u8URL = "https://hls.videocc.net/c7d3982d0d/f/c7d3982d0d4360a289c58754dbdfd80f_2.m3u8?pid=1563756868775X1523162&device=desktop"
     oriURL = "https://www.nowcoder.com/study/vod/1041/10/1"
-    videoId, keyRequestParam = getKeyRequestParam(oriURL, downloader.headers)
+    videoId, keyRequestParam = getKeyRequestParam(oriURL,downloader.headers)
+    m3u8URL = "https://hls.videocc.net/" + videoId[:catcount] + "/f/" + videoId[:-2] + "_2.m3u8?device=desktop"
+
     post_params = {
         "token": keyRequestParam
     }
